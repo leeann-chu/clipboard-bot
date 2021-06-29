e22477f2d51ed6035ac886417d6423e8f2608ef5
lchu7 <lchu7@u.rochesr.edu>
2021-06-29 04:10:56 -0400

    die

--- cogs/menusUtil.py
+++ cogs/menusUtil.py
@@ -18,7 +18,10 @@ class Confirm(menus.Menu):
         self.result = None
 
     async def send_initial_message(self, ctx):
-        return await ctx.send(embed = self.msg)
+        if not self.msg.embeds:
+            return await ctx.send(embed = self.msg)
+        else:
+            return await ctx.send(self.msg)
 
     @menus.button('<:cancel:851278899270909993>')
     async def do_deny(self, payload):
@@ -33,22 +36,61 @@ class Confirm(menus.Menu):
     async def prompt(self, ctx):
         await self.start(ctx, wait=True)
         return self.result
-    
-#➥ Setting up an Viewing Notes Menu
+##
+        
+#➥ Setting up Viewing Notes Menu
 class ViewNotes(menus.ListPageSource):
     def __init__(self, data):
         super().__init__(data, per_page=9)
     
-    async def format_page(self, menu, titleList, optionList, tagList):
+    async def format_page(self, menu, entries): 
         embed = discord.Embed(
-            title = "Your Notes: ",
+            title = "Your Saved Notes",
             description = "Enter a number to view selected note",
             color = randomHexGen()
         )
-        #embed.add_field(name = entry + "Title: " + entry, value = entry)
-        embed.set_footer(text = f'\nPage {menu.current_page + 1}/{self.get_max_pages()}')
-        return '\n'.join(f'{i}. {v.title}' for i, v in enumerate(titleList, start = 1))
+        embed.set_footer(text = f'\nPage {menu.current_page + 1}/{self.get_max_pages()}' + " | React with ❌ to cancel option selection")
+        for entry in entries:
+            embed.add_field(name = entry.option + " " + entry.title, value = entry.tag)        
+        return embed
 ##
-    
+        
+#➥ Setting up Deleting Notes Menu
+class DeleteNotes(menus.MenuPages):
+    def __init__(self, source):
+        super().__init__(source = source, timeout = 60, delete_message_after=True)
+        
+    async def finalize(self, timed_out):
+        try:
+            if timed_out:
+                await self.message.clear_reactions()
+            else:
+                await self.message.delete()
+        except discord.HTTPException:
+            pass
+        
+    @menus.button('<:cancel:851278899270909993>', position=menus.Last(0)) 
+    async def on_cancel(self, payload):
+        self.stop()
+##
+
+#➥ Setting up Menu Page Source
+class EmbedFormatSource(menus.ListPageSource):
+    def __init__(self, entries, per_page):
+        super().__init__(entries, per_page=per_page)
+            
+    async def format_page(self, menu, entries): 
+            embed = discord.Embed(
+                title = "Selected Note(s) to be Deleted: \n" + entries[0].title,
+                description = "Enter a number to delete selected note",
+                color = randomHexGen()
+            )
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/519350010706395157/850574601623961640/full_cross.png")
+            embed.set_footer(text = f'\nPage {menu.current_page + 1}/{self.get_max_pages()}' + " | React with ❌ to cancel option selection")
+            for entry in entries:
+                embed.add_field(name = entry.option + "\n__" + entry.tag + "__", value = entry.content)        
+            return embed
+##
+
 def setup(bot):
     bot.add_cog(menusUtil(bot))
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import discord
-import os
 import random
 import json
 import asyncio
@@ -9,9 +8,10 @@ from discord.ext import commands
 from datetime import datetime
 
 description = "I can store long term, short term, and immediate goals!"
-credentials = {"user": USERNAME, "password": PASSWORD, "database": DATABASE, "host": "127.0.0.1"}
+credentials = {"user": USERNAME, "password": PASSWORD,
+               "database": DATABASE, "host": "127.0.0.1"}
 
-#➥ randomHexGen
+# ➥ randomHexGen
 def randomHexGen():
     # Create a random hex value
     return random.randint(0, 16777215)
@@ -24,20 +24,20 @@ def get_prefix(bot, message):
         with open("prefixes.json", 'r') as f:
             prefixes = json.load(f)
         return prefixes[str(message.guild.id)]
-    
+
 loop = asyncio.get_event_loop()
 db = loop.run_until_complete(asyncpg.create_pool(**credentials))
-bot = commands.Bot(command_prefix=get_prefix, description=description, activity=discord.Activity(type= discord.ActivityType.listening, name="you forget your milk"), db=db)   
+bot = commands.Bot(command_prefix=get_prefix, description=description, activity=discord.Activity(
+    type=discord.ActivityType.listening, name="you forget your milk"), db=db)
 
-
-#➥ on ready command
+# ➥ on ready command
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
     print("------------------------------")
 ##
 
-#➥ .json manipulation
+# ➥ .json manipulation
 @bot.event
 async def on_guild_join(guild):
     with open("prefixes.json", 'r') as f:
@@ -45,17 +45,18 @@ async def on_guild_join(guild):
     # Default guild value, the prefix all servers should start with
     prefixes[str(guild.id)] = '~'
     with open("prefixes.json", 'w') as f:
-        json.dump(prefixes, f, indent = 4)
+        json.dump(prefixes, f, indent=4)
+
 @bot.event
 async def on_guild_remove(guild):
     with open("prefixes.json", 'r') as f:
         prefixes = json.load(f)
     prefixes.pop(str(guild.id))
     with open("prefixes.json", 'w') as f:
-        json.dump(prefixes, f, indent = 4)
+        json.dump(prefixes, f, indent=4)
 ##
 
-#➥ Prefix Checker
+# ➥ Prefix Checker
 @bot.event
 async def on_message(message):
     prefix = get_prefix(bot, message)
@@ -66,17 +67,17 @@ async def on_message(message):
     await bot.process_commands(message)
 ##
 
-#➥ Help command
+# ➥ Help command
 @bot.command()
-async def chelp(ctx, argument = None):
+async def chelp(ctx, argument=None):
     await ctx.trigger_typing()
 
     # Get server prefix
     prefix = ctx.prefix
     member = ctx.message.author
     m = ctx.message
-    
-    #➥ Get hover text
+
+    # ➥ Get hover text
     with open("helpmenu.json", 'r') as f:
         helpmenu = json.load(f)
 
@@ -87,36 +88,37 @@ async def chelp(ctx, argument = None):
     repeat = helpmenu["repeat"]
     _8ball = helpmenu["8ball"]
     ##
-        
+
     if argument is None:
-#➥ Embed for Empty Argument
+        # ➥ Embed for Empty Argument
         embed = discord.Embed(
-            description = f"Help menu for all your clipboard commands\nHover over the command to see more info or type `{prefix}chelp [command]` for more help",
-            color = randomHexGen(),
-            timestamp = datetime.utcnow()
+            description=f"Help menu for all your clipboard commands\nHover over the command to see more info or type `{prefix}chelp [command]` for more help",
+            color=randomHexGen(),
+            timestamp=datetime.utcnow()
         )
 
-        embed.set_author(name="Here to help!", icon_url="https://cdn.discordapp.com/attachments/809686249999826955/845595120639672320/bigBirdy.gif")
-        embed.add_field(name="<a:settings:845834409869180938> Useful Commands", 
-            value=(f"""[`{prefix}avatar`](https://www.tumblr.com/blog/view/magnificenttyger "{avatar}") ⇀ Returns user's avatar
+        embed.set_author(
+            name="Here to help!", icon_url="https://cdn.discordapp.com/attachments/809686249999826955/845595120639672320/bigBirdy.gif")
+        embed.add_field(name="<a:settings:845834409869180938> Useful Commands",
+                        value=(f"""[`{prefix}avatar`](https://www.tumblr.com/blog/view/magnificenttyger "{avatar}") ⇀ Returns user's avatar
                     [`{prefix}clear x`](https://www.tumblr.com/blog/view/magnificenttyger "{clear}") ⇀ Clears x number of messages (default is 10)
                         `{prefix}info` ⇀ Tells you more about this bot
                         `{prefix}joined` ⇀ Returns info about when user joined
                         `{prefix}ping` ⇀ Returns ping
                         `{prefix}prefix` ⇀ Edit the prefix used for commands on this server
-                    """), 
-                    inline=True)
-        embed.add_field(name="<a:pugpls:846829754036256808> Fun Commands", 
-            value=f"""[`{prefix}add`](https://www.tumblr.com/blog/view/magnificenttyger "{add}") ⇀ Adds two numbers together, takes two arguments 
+                    """),
+                        inline=True)
+        embed.add_field(name="<a:pugpls:846829754036256808> Fun Commands",
+                        value=f"""[`{prefix}add`](https://www.tumblr.com/blog/view/magnificenttyger "{add}") ⇀ Adds two numbers together, takes two arguments 
                     [`{prefix}repeat`](https://www.tumblr.com/blog/view/magnificenttyger "{repeat}") ⇀ Repeats user input
                     [`{prefix}8ball`](https://www.tumblr.com/blog/view/magnificenttyger "{_8ball}") ⇀ Ask <:8ball:845546744665735178> questions   
-                    """,        
-                    inline=True)
+                    """,
+                        inline=True)
 
         await ctx.send(embed=embed)
     ##
     elif argument in bot.all_commands:
-    #➥ Help per command
+        # ➥ Help per command
         command = bot.get_command(argument)
         if (argument) == "avatar":
             await ctx.send(f"{avatar}")
@@ -128,33 +130,34 @@ async def chelp(ctx, argument = None):
             await ctx.send(f"{repeat}")
         elif (argument) == "8ball":
             await ctx.send(f"{_8ball}")
-    ##        
+    ##
     else:
         await ctx.send("Unrecognized command")
-##    
+##
 
-#➥ Close Bot
+# ➥ Close Bot
 @bot.command()
 async def quit(ctx):
     if await bot.is_owner(ctx.author):
-        await bot.change_presence(status = discord.Status.offline)
-        await db.close()
+        await bot.change_presence(status=discord.Status.offline)
+        await ctx.send("Bot is Closed!")
         await bot.logout()
+        await db.close()
     else:
         await ctx.send("You do not have the permissions to use this command!")
 ##
 
-#➥ Ping
+# ➥ Ping
 @bot.command()
 async def ping(ctx):
     await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
 ##
 
-#➥ Info command
+# ➥ Info command
 @bot.command()
 async def info(ctx):
     await ctx.trigger_typing()
-    
+
     # Get my current profile pic
     member = await ctx.guild.fetch_member("364536918362554368")
     pfp = member.avatar_url
@@ -164,18 +167,19 @@ async def info(ctx):
 
     # Create Embed
     embed = discord.Embed(
-        title = "📋 Clipboard Bot Information", 
-        description = description + f"\nThis server's prefix is `{prefix}`", 
-        color = randomHexGen()
+        title="📋 Clipboard Bot Information",
+        description=description + f"\nThis server's prefix is `{prefix}`",
+        color=randomHexGen()
     )
     #embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-    embed.add_field(name="__Functionalities__", value="• timer to send a reminder \n• check things off the list \n• create sticky reminders \n• create persistent reminders", inline=False)
+    embed.add_field(name="__Functionalities__",
+                    value="• timer to send a reminder \n• check things off the list \n• create sticky reminders \n• create persistent reminders", inline=False)
     embed.set_footer(text="Created by GracefulLion", icon_url=pfp)
 
-    await ctx.send(embed = embed)
+    await ctx.send(embed=embed)
 ##
 
-#➥ loading and unloading
+# ➥ loading and unloading
 @bot.command(name="reload")
 @commands.is_owner()
 async def reload(ctx, *, extension: str):
@@ -186,19 +190,19 @@ async def reload(ctx, *, extension: str):
         print(f'{extension} is reloaded!')
         await ctx.send(f'Extension {extension} is reloaded!')
 
-for filename in os.listdir("./cogs"):
-    if filename.endswith('.py'):
-        #takes .py files and cuts off their extension
-        bot.load_extension(f'cogs.{filename[:-3]}')
+cogsList = ["botFun", "clipboard", "error_handler", "menusUtil", "utilities"]
+for cog in cogsList:
+    bot.load_extension(f'cogs.{cog}')
 
 @reload.error
 async def reload_error(ctx, error):
     if isinstance(error, commands.CommandInvokeError):
         error = error.original
-    extensionErrors = (commands.ExtensionNotLoaded, commands.ExtensionNotFound, commands.MissingRequiredArgument, )
+    extensionErrors = (commands.ExtensionNotLoaded,
+                       commands.ExtensionNotFound, commands.MissingRequiredArgument, )
     if isinstance(error, extensionErrors):
         await ctx.send(f"Unrecognized Extension!")
-    else: 
+    else:
         print("\nSome other Error!")
         raise error
 
