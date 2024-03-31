a4421d5cc70b79f5e1c7f02a9aefde505343d6c6
LeeAnn <80134915+leeann-chu@users.noreply.github.com>
2024-03-31 14:34:31 -0400

    definitely dont' remember anymore

--- cogs/category_org.py
+++ cogs/category_org.py
@@ -1,50 +1,49 @@
-import csv
-import random
 import discord
+from random import shuffle
 from discord.ext import commands
-from discord.ext.commands import TextChannelConverter, VoiceChannelConverter, CategoryChannelConverter, RoleConverter
+from discord.ext.commands import TextChannelConverter, CategoryChannelConverter, RoleConverter
 
 #remember to save your csv without the UTF encoding
-channel_id = {
-    "icon_idea"          : "370200859675721732",
-    "general_category"   : "370200859675721730",
-    "voice_category"     : "370200859675721734",
-    "gaming_category"    : "966833708189487165",
-    "interests_category" : "811036378501873695",
-    "nsfw_category"      : "966833499166343198",
-    "general"            : "370200859675721732",
-    "memes"              : "370260333425721347",
-    "art"                : "370260576192036865",
-    "counting"           : "388542578779226112",
-    "bot"                : "388133097851453440",
-    "general_vc"         : "878073222178959370",
-    "virgin_vc"          : "485329660062728212",
-    "chad_vc"            : "485329708033114113",
-    "afk_vc"             : "388493850563575818",
-    "vidya"              : "464281055466225664",
-    "pets"               : "829568327915405323",
-    "minecraft"          : "716338107594702919",
-    "tech"               : "869235656365318185",
-    "politics"           : "813943234110685234",
-    "guns"               : "456508133913788436",
-    "fit"                : "962927526550851614",
-    "documents"          : "811036333962559500",
-    "cooking"            : "1030154745022787665",
-    "anime"   : "377274787740909579",
-    "role1"   : "871957977370878052",
-    "role2"   : "871958094899462154",
-    "role3"   : "871958129666060298",
-    "role4"   : "871958159168782357",
-    "botrole" : "388833071908257793"
-}
+# channel_id = {
+#     "icon_idea"          : "370200859675721732",
+#     "general_category"   : "370200859675721730",
+#     "voice_category"     : "370200859675721734",
+#     "gaming_category"    : "966833708189487165",
+#     "interests_category" : "811036378501873695",
+#     "nsfw_category"      : "966833499166343198",
+#     "general"            : "370200859675721732",
+#     "memes"              : "370260333425721347",
+#     "art"                : "370260576192036865",
+#     "counting"           : "388542578779226112",
+#     "bot"                : "388133097851453440",
+#     "general_vc"         : "878073222178959370",
+#     "virgin_vc"          : "485329660062728212",
+#     "chad_vc"            : "485329708033114113",
+#     "afk_vc"             : "388493850563575818",
+#     "vidya"              : "464281055466225664",
+#     "pets"               : "829568327915405323",
+#     "minecraft"          : "716338107594702919",
+#     "tech"               : "869235656365318185",
+#     "politics"           : "813943234110685234",
+#     "guns"               : "456508133913788436",
+#     "fit"                : "962927526550851614",
+#     "documents"          : "811036333962559500",
+#     "cooking"            : "1030154745022787665",
+#     "anime"   : "377274787740909579",
+#     "role1"   : "871957977370878052",
+#     "role2"   : "871958094899462154",
+#     "role3"   : "871958129666060298",
+#     "role4"   : "871958159168782357",
+#     "botrole" : "388833071908257793"
+# }
 
-category_list = ["general_category", "voice_category", "gaming_category", "interests_category", "nsfw_category"]
-channel_list = ["general", "memes", "art", "counting", "bot", "vidya", "pets", "minecraft", "tech", "politics", "guns", "fit", "documents", "cooking", "anime"]
-voice_list = ["general_vc", "virgin_vc", "chad_vc", "afk_vc"]
-role_list = ["role1", "role2", "role3", "role4", "botrole"]
+# category_list = ["general_category", "voice_category", "gaming_category", "interests_category", "nsfw_category"]
+# channel_list = ["general", "memes", "art", "counting", "bot", "vidya", "pets", "minecraft", "tech", "politics", "guns", "fit", "documents", "cooking", "anime"]
+# voice_list = ["general_vc", "virgin_vc", "chad_vc", "afk_vc"]
+# role_list = ["role1", "role2", "role3", "role4", "botrole"]
 
 def partition (members_list, n):
-    random.shuffle(members_list)
+    shuffle(members_list)
     return [members_list[i::n] for i in range(n)]
 
 class category_org(commands.Cog):
@@ -57,51 +56,51 @@ class category_org(commands.Cog):
         print("category_org is Ready")
 
     # Enforcing Theme Change
-    @commands.command() 
-    @commands.has_permissions(manage_guild=True)
-    async def changeTheme(self, ctx):
-        # contents = await ctx.message.attachments[0].read()
-        try: await ctx.message.attachments[0].save("data/serverTheme.csv")
-        except IndexError:
-            return await ctx.send("You forgot to include an attachment!")
-        # with open(contents, 'r') as csv_file:
-        with open('data/serverTheme.csv', 'r') as csv_file:
-            for (default, new) in csv.reader(csv_file):
-                default = default.strip(' "\'\t\r\n')
-                new = new.strip(' "\'\t\r\n')
-                channel_from_dict = channel_id[default]
-                if new == "":
-                    continue
-                if default in channel_list:
-                    channel_obj = await TextChannelConverter().convert(ctx, channel_from_dict)
-                    await channel_obj.edit(name=new)
-                    continue
-                if default in role_list:
-                    role_obj = await RoleConverter().convert(ctx, channel_from_dict)
-                    await role_obj.edit(name=new)
-                    continue 
-                if default in category_list:
-                    category_obj = await CategoryChannelConverter().convert(ctx, channel_from_dict)
-                    await category_obj.edit(name=new)
-                    continue
-                if default in voice_list:
-                    vc_obj = await VoiceChannelConverter().convert(ctx, channel_from_dict)
-                    await vc_obj.edit(name=new)
-                    continue
-                if default == "icon_idea":
-                    channel_obj = await TextChannelConverter().convert(ctx, channel_from_dict)
-                    await channel_obj.send(f"Submitted Icon Prompt: {new}")
-                    continue
+    # @commands.command() 
+    # @commands.has_permissions(manage_guild=True)
+    # async def changeTheme(self, ctx):
+    #     # contents = await ctx.message.attachments[0].read()
+    #     try: await ctx.message.attachments[0].save("data/serverTheme.csv")
+    #     except IndexError:
+    #         return await ctx.send("You forgot to include an attachment!")
+    #     # with open(contents, 'r') as csv_file:
+    #     with open('data/serverTheme.csv', 'r') as csv_file:
+    #         for (default, new) in csv.reader(csv_file):
+    #             default = default.strip(' "\'\t\r\n')
+    #             new = new.strip(' "\'\t\r\n')
+    #             channel_from_dict = channel_id[default]
+    #             if new == "":
+    #                 continue
+    #             if default in channel_list:
+    #                 channel_obj = await TextChannelConverter().convert(ctx, channel_from_dict)
+    #                 await channel_obj.edit(name=new)
+    #                 continue
+    #             if default in role_list:
+    #                 role_obj = await RoleConverter().convert(ctx, channel_from_dict)
+    #                 await role_obj.edit(name=new)
+    #                 continue 
+    #             if default in category_list:
+    #                 category_obj = await CategoryChannelConverter().convert(ctx, channel_from_dict)
+    #                 await category_obj.edit(name=new)
+    #                 continue
+    #             if default in voice_list:
+    #                 vc_obj = await VoiceChannelConverter().convert(ctx, channel_from_dict)
+    #                 await vc_obj.edit(name=new)
+    #                 continue
+    #             if default == "icon_idea":
+    #                 channel_obj = await TextChannelConverter().convert(ctx, channel_from_dict)
+    #                 await channel_obj.send(f"Submitted Icon Prompt: {new}")
+    #                 continue
 
-                await ctx.send(f"Error: `{default}` is unrecognized")
+    #             await ctx.send(f"Error: `{default}` is unrecognized")
 
-                    # print(f"id: {channel_id[default.strip()]} new: {new.strip()}")
-    @changeTheme.error
-    async def changeTheme_error(self, ctx, error):
-        if isinstance(error, discord.errors.Forbidden):
-            await ctx.send(error)
-        else:
-            await ctx.send(error)
+    #                 # print(f"id: {channel_id[default.strip()]} new: {new.strip()}")
+    # @changeTheme.error
+    # async def changeTheme_error(self, ctx, error):
+    #     if isinstance(error, discord.errors.Forbidden):
+    #         await ctx.send(error)
+    #     else:
+    #         await ctx.send(error)
     
     # Shuffle Members
     @commands.command()
@@ -189,6 +188,7 @@ class category_org(commands.Cog):
         for channel in category_obj.channels:
             await ctx.guild.create_role(name=channel.name)
         await ctx.send(f"Roles created for channels in {category_obj}")
+
     # Mass change channel permissions to match role name and makes channel private
     @commands.command()
     @commands.has_permissions(manage_guild=True)
@@ -238,6 +238,6 @@ class category_org(commands.Cog):
         await ctx.send("Channel name updated!")
 
 
-# async def setup(bot):
-#     await bot.add_cog(category_org(bot))
+async def setup(bot):
+    await bot.add_cog(category_org(bot))
     
