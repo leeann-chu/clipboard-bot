3091d702924fa725af64867852cf04fe2b8ad7af
LeeAnn <80134915+leeann-chu@users.noreply.github.com>
2024-08-05 10:08:08 -0400

    finall changed it from display_name to member.name in poll results

--- cogs/embedBuilder.py
+++ cogs/embedBuilder.py
@@ -462,7 +462,7 @@ class embedBuilder(commands.Cog):
         self.watch_fic_task.start()
 
     # Loop
-    @tasks.loop(minutes=25)
+    @tasks.loop(hours=11)
     async def watch_fic_task(self):
         alertDB = readfromFile("alertMe")
         for key in alertDB:
